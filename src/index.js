bcb532c12a9e76aacd5cc8a7fc7513e3758c42d2
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-10-02 13:09:02 +0300

    refactor getLoveTrianglesCount function

--- src/index.js
+++ src/index.js
@@ -3,26 +3,8 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  let triangleCount = 0;
-  let prefLength = preferences.length;
+  const loves = preferences;
+  const triangles = loves.filter((love, i) => loves[loves[love - 1] - 1] === i + 1);
 
-  const wrongPref = (a, b) => a < 1 || a === b || a > prefLength;
-
-  for (let i = 1; i <= prefLength; i++) {
-
-    let love1 = preferences[i-1];
-    let love2 = preferences[love1-1];
-    let love3 = preferences[love2-1];
-
-    if (wrongPref(love1, i) || wrongPref(love2, love1) || wrongPref(love3, love2)) {
-      continue;
-    }
-
-    if (love3 === i) {
-      triangleCount += 1;
-      preferences[love2-1] = -1;
-    }
-  }
-
-  return triangleCount;
+  return Math.floor(triangles.length / 3);
 };
